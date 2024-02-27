cdc065fd7cd975b7711cf6e93043cd894fca0a90
Jalussa dos Santos <jalussa_santos@sicredi.com.br>
2024-02-27 12:55:14 -0300

    deleção logica de dados

--- spring-boot-api/src/main/java/com/formacao/springbootapi/model/medico/Medico.java
+++ spring-boot-api/src/main/java/com/formacao/springbootapi/model/medico/Medico.java
@@ -33,7 +33,11 @@ public class Medico {
   private Especialidade especialidade;
   @Embedded
   private Endereco endereco;
+  private Boolean ativo;
+
   public Medico(CadastroMedicoDto medico) {
+
+    this.ativo = true;
     this.nome = medico.nome();
     this.email = medico.email();
     this.crm = medico.crm();
@@ -44,14 +48,18 @@ public class Medico {
 
   public void atualizarInformacoes(DadosAtualizacaoMedicoDto dados) {
 
-      if (dados.nome() != null) {
-        this.nome = dados.nome();
-      }
-      if (dados.telefone() != null) {
-        this.telefone = dados.telefone();
-      }
-      if (dados.endereco() != null) {
-        this.endereco.atualizarInformacoes(dados.endereco());
-      }
+    if (dados.nome() != null) {
+      this.nome = dados.nome();
+    }
+    if (dados.telefone() != null) {
+      this.telefone = dados.telefone();
+    }
+    if (dados.endereco() != null) {
+      this.endereco.atualizarInformacoes(dados.endereco());
+    }
+  }
+
+  public void excluir() {
+    this.ativo = false;
   }
 }
